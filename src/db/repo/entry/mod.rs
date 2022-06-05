6aa0cd0251ef0eeb4d22314e1c75c61e0efcaf18
baris-inandi <68742481+baris-inandi@users.noreply.github.com>
2022-06-06 00:14:24 +0300

    commit

--- src/db/repo/entry/mod.rs
+++ src/db/repo/entry/mod.rs
@@ -10,3 +10,10 @@ pub struct Entry {
     pub identifier: String,
     pub repo_name: String,
 }
+
+impl std::fmt::Display for Entry {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        let out = String::from("");
+        return out.fmt(f);
+    }
+}
